02715f7ae1dda825649f2ff662c781d9ac572afb
sandro gagoshidze <49387424+sannyybg@users.noreply.github.com>
2020-06-24 19:54:36 +0400

    Add files via upload

---
+++ Controllers/StoreController.cs
@@ -0,0 +1,42 @@
+﻿using muscshop.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace muscshop.Controllers
+{
+    public class StoreController : Controller
+    {
+        // private static FakeDatabase _database = new FakeDatabase();
+
+        StoreContext _storeContext = new StoreContext();
+        // GET: Store
+        public ActionResult Index()
+        {
+            var genres = _storeContext.Genres;
+            // var genres = _database.Genres;
+            return View(genres);
+        }
+
+        public ActionResult Browse(string genre)
+        {
+            var genreModel = _storeContext.Genres.Where(x => x.Name == genre).FirstOrDefault();
+
+            var albums = _storeContext.Albums.Where(x => x.Genre.Name == genre).ToList();
+
+            genreModel.Albums = albums;
+
+            return View(genreModel);
+        }
+
+        public ActionResult Detail(int id)
+        {
+            var album = _storeContext.Albums.Where(x => x.AlbumId == id).FirstOrDefault();
+
+
+            return View(album);
+        }
+    }
+}
